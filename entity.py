d1e8446b18d32e488c4c73defd13c833400d2055
charliehawley <charlie.e.hawley@gmail.com>
2021-11-13 18:07:20 +0000

    add function to entity.py to initialise the hero

--- entity.py
+++ entity.py
@@ -7,3 +7,18 @@ class Ent:
         self.power = power
         self.icon = name[0]
         self.position = position
+
+hero_position = [(randint(0, 10)), (randint(0, 10))]
+
+def hero_init():
+    while True:
+        try: 
+            h_name = input('What is your name hero?\n')
+            if h_name[0].isalpha():
+                hero = Ent(h_name, 15, 3, hero_position)
+                return hero
+                break
+            else:
+                print("That's an unusual name, would you mind anglicizing it...")
+        except IndexError as e:
+            print('You must have a name?')
