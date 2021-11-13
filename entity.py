1c97ddde4477deb8118b9611c76f83b1196435e0
charliehawley <charlie.e.hawley@gmail.com>
2021-11-13 20:56:35 +0000

    add method for generating entity attributes for "enemy", "loot" and "stairs"

--- entity.py
+++ entity.py
@@ -21,5 +21,33 @@ def hero_init():
             else:
                 print("That's an unusual name, would you mind anglicizing it...")
         except IndexError as e:
-            print('You must have a name?')
+            print('But you must have a name?')
 
+def ents_init(enemy_ent, loot_ent):
+    enemy1_position = [(randint(0, 10)), (randint(0, 10))]
+
+    a = hero_position
+    b = enemy1_position
+
+    while a == b:
+        enemy1_position = [(randint(0, 10)), (randint(0, 10))]
+
+    enemy_ent = Ent('#' + enemy_ent[0], enemy_ent[1], enemy_ent[2], enemy1_position)
+
+    loot1_position = [(randint(0, 10)), (randint(0, 10))]
+    c = loot1_position
+
+    while c == a or c == b:
+        loot1_position = [(randint(0, 10)), (randint(0, 10))]
+
+    loot_ent = Ent('?' + loot_ent[0], loot_ent[1], loot_ent[2], loot1_position)
+
+    stairs1_position = [(randint(0, 10)), (randint(0, 10))]
+    d = stairs1_position
+
+    while d == a or d == b or d == c:
+        stairs1_position = [(randint(0, 10)), (randint(0, 10))]
+
+    stairs = Ent('/stairs', 0, 0, stairs1_position)
+
+    return vars(enemy_ent), vars(loot_ent), vars(stairs)
--- run.py
+++ run.py
@@ -8,13 +8,12 @@ ent_dict = {'runt': ['runt', 3, 2], 'bruiser': ['bruiser', 4, 3], 'boss': ['boss
 
 ent_names = list(ent_dict.keys())
 
-l1 = [ent_dict['runt'], ent_dict[ent_names[randint(3,5)]]]
-l2 = [ent_dict['bruiser'], ent_dict[ent_names[randint(3,5)]]]
-l3 = [ent_dict['boss'], ent_dict[ent_names[randint(3,5)]]]
-
-rand_loot = ent_names[randint(3,5)]
+l1 = [ent_dict['runt'], ent_dict[ent_names[randint(3, 5)]]]
+l2 = [ent_dict['bruiser'], ent_dict[ent_names[randint(3, 5)]]]
+l3 = [ent_dict['boss'], ent_dict[ent_names[randint(3, 5)]]]
 
 print(l1)
 print(l2)
 print(l3)
-# ents = ents_init(l1)
+
+ents = entity.ents_init(l1[0], l1[1])
