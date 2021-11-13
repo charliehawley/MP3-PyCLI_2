ba5aef356969827b7712c9cb98d596f64ce85c96
charliehawley <charlie.e.hawley@gmail.com>
2021-11-13 22:05:08 +0000

    add ents to map in print_map function (level.py, lines 12-15)

--- level.py
+++ level.py
@@ -6,11 +6,14 @@ def largest_index_position(lst1, lst2, lst3, lst4):
 
 class level_map_gen:
     def __init__(self, int):
-        self.size = randint(5, int)
-        self.map = [['-' for x in range(0, self.size)] for x in range(0, self.size)]
+        self.map = [['-' for x in range(0, int + 1)] for x in range(0, int + 1)]
     
-    def print_map(self, lst_lst, hp, dmg):
+    def print_map(self, lst_lst, hp, dmg, hero_pos, enemy_pos, loot_pos, stairs_pos, hero_name ):
+        lst_lst[hero_pos[0]][hero_pos[1]] = hero_name[0]
+        lst_lst[enemy_pos[0]][enemy_pos[1]] = '#'
+        lst_lst[loot_pos[1]][loot_pos[0]] = '?'
+        lst_lst[stairs_pos[0]][stairs_pos[1]] = '/'
         for i in lst_lst:
             print('  '.join(i))
         print(f'HP: {hp}   Dmg: {dmg}')
-        print('% = stairs, # = enemy, * = loot')
+        print('# = enemy, ? = loot, / = stairs')
--- run.py
+++ run.py
@@ -4,8 +4,7 @@ from random import randint
 
 hero = entity.hero_init()
 
-ent_dict = {'runt': ['runt', 3, 2], 'bruiser': ['bruiser', 4, 3], 'boss': ['boss', 10, 4], 'lunch': ['lunch',0, 5], 
-'stimulant': ['stimulant', 0, 2], 'vigour': ['vigour', 0, 5]}
+ent_dict = {'runt': ['runt', 3, 2], 'bruiser': ['bruiser', 4, 3], 'boss': ['boss', 10, 4], 'lunch': ['lunch', 0, 5], 'stimulant': ['stimulant', 0, 2], 'vigour': ['vigour', 0, 5]}
 
 ent_names = list(ent_dict.keys())
 
@@ -14,8 +13,12 @@ l2 = [ent_dict['bruiser'], ent_dict[ent_names[randint(3, 5)]]]
 l3 = [ent_dict['boss'], ent_dict[ent_names[randint(3, 5)]]]
 
 enemy, loot, stairs = entity.ents_init(l1[0], l1[1])
+print(hero.position)
+print(enemy.position)
+print(loot.position)
+print(stairs.position)
 
 map_limit = level.largest_index_position(hero.position, enemy.position, loot.position, stairs.position)
+print(map_limit)
 level_map = level.level_map_gen(map_limit)
-level_map.print_map(level_map.map, hero.hp, hero.power)
-# print(level_map)
+level_map.print_map(level_map.map, hero.hp, hero.power, hero.position, enemy.position, loot.position, stairs.position, hero.name)
