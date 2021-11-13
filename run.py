215f3a572cb4bd652f93d985f9d584a115f9700d
charliehawley <charlie.e.hawley@gmail.com>
2021-11-13 19:19:45 +0000

    add lists for each level from dictionary for generating ents

--- run.py
+++ run.py
@@ -3,4 +3,18 @@ from random import randint
 
 hero = entity.hero_init()
 
-ent_dict = {'runt': [3, 2], 'boss': [10, 4], 'lunch': [0, 5], 'stimulant': [0, 2], 'vigour': [0, 5]}
+ent_dict = {'runt': ['runt', 3, 2], 'bruiser': ['bruiser', 4, 3], 'boss': ['boss', 10, 4], 'lunch': ['lunch',0, 5], 
+'stimulant': ['stimulant', 0, 2], 'vigour': ['vigour', 0, 5]}
+
+ent_names = list(ent_dict.keys())
+
+l1 = [ent_dict['runt'], ent_dict[ent_names[randint(3,5)]]]
+l2 = [ent_dict['bruiser'], ent_dict[ent_names[randint(3,5)]]]
+l3 = [ent_dict['boss'], ent_dict[ent_names[randint(3,5)]]]
+
+rand_loot = ent_names[randint(3,5)]
+
+print(l1)
+print(l2)
+print(l3)
+# ents = ents_init(l1)
